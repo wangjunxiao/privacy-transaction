224a71bf96415e04ee9d28bff361da8e241f70bc
Junxiao Wang <wangjunxiao@live.com>
2021-11-08 10:06:13 +0800

    updated

--- dataloader/gaussian.py
+++ dataloader/gaussian.py
@@ -1,19 +1,35 @@
 import numpy as np
 
-n1 = 2*10**2
-n2 = 4*10**2
-n3 = 8*10**2
-n = n1 + n2 + n3
-cov = [[0.05,0],[0,0.05]]
+n1 = 1*10**2
+n2 = 2*10**2
+n3 = 2*10**2
+n4 = 1*10**2
+n5 = 2*10**2
+n = n1 + n2 + n3 + n4 + n5
+cov1 = [[0.3,0],[0,0.3]]
+cov2 = [[0.3,0],[0,0.3]]
+cov3 = [[0.3,0],[0,0.3]]
+cov4 = [[0.3,0],[0,0.3]]
+cov5 = [[0.4,0],[0,0.5]]
 
 def loader():
     # synthetic dataset made from a Gaussian mixture
-    cluster1 = np.random.multivariate_normal([2,0], cov, n1)
-    cluster2 = np.random.multivariate_normal([0,2], cov, n2)
-    cluster3 = np.random.multivariate_normal([-1.5,-1.5], cov, n3)
+    center1 = np.array([1.5,0])
+    center2 = np.array([0,1.5])
+    center3 = np.array([-1.5,-1.5])
+    center4 = np.array([0.6,-1.0])
+    center5 = np.array([-1.5,1.5])
+    cluster1 = np.random.multivariate_normal(center1, cov1, n1)
+    cluster2 = np.random.multivariate_normal(center2, cov2, n2)
+    cluster3 = np.random.multivariate_normal(center3, cov3, n3)
+    cluster4 = np.random.multivariate_normal(center4, cov4, n4)
+    cluster5 = np.random.multivariate_normal(center5, cov5, n5)
     labels1 = np.array([0]*n1)
     labels2 = np.array([1]*n2)
     labels3 = np.array([2]*n3)
-    data = np.vstack((cluster1, cluster2, cluster3))
-    labels = np.concatenate((labels1, labels2, labels3))
+    labels4 = np.array([3]*n4)
+    labels5 = np.array([4]*n5)
+    #centers = np.vstack((center1, center2, center3, center4, center5))
+    data = np.vstack((cluster1, cluster2, cluster3, cluster4, cluster5))
+    labels = np.concatenate((labels1, labels2, labels3, labels4, labels5))
     return labels, data
---
+++ evaluate/internal/sc.py
@@ -0,0 +1,45 @@
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+#Silhouette Coefficient
+def eval_sc(data, assignments):
+    # The silhouette_score gives the average value for all the samples.
+    # This gives a perspective into the density and separation of the formed clusters
+    silhouette_avg = silhouette_score(data, assignments)
+    return silhouette_avg
+    
+def test():
+    # Generating the sample data from make_blobs
+    # This particular setting has one distinct cluster and 3 clusters placed close
+    # together.
+    X, y = make_blobs(
+        n_samples=500,
+        n_features=2,
+        centers=4,
+        cluster_std=1,
+        center_box=(-10.0, 10.0),
+        shuffle=True,
+        random_state=1,
+    )  # For reproducibility
+    
+    range_n_clusters = [4]
+
+    for n_clusters in range_n_clusters:
+        # Initialize the clusterer with n_clusters value and a random generator
+        # seed of 10 for reproducibility.
+        clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+        cluster_labels = clusterer.fit_predict(X)
+        print(len(cluster_labels))
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed clusters
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        print(
+            "For n_clusters =",
+            n_clusters,
+            "The average silhouette_score is :",
+            silhouette_avg,
+        )
+    
+if __name__=='__main__':
+    test()
